91f0697598c6b026d8d21c3e6171a56b24085c1a
Irina1515 <o951@mail.ru>
2020-04-01 19:33:19 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,20 @@
+import sys
 import os
 
 if __name__ == "__main__":
-
+    st = 'error'
+    if len(sys.argv) == 2:
+        st = sys.argv[1]
+    if len(sys.argv) > 2:
+        print("Incorrect number of params!")
+        sys.exit(1)
+    data = []
     d = "./data"
     for filename in os.listdir(d):
         print('open ' + filename)
         with open(d + '/' + filename) as f:
             for line in f:
+                if line.find(st) != -1:
+                    data.append(line)
                     print(line)
     f.close()
